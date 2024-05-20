f349ef13bbf2ed022897f6eb6e0c350db7dd8fd7
yan3ku <wisniewskijas@outlook.com>
2024-05-20 20:06:02 +0200

    Add comments and stuff.

--- klip.sh
+++ klip.sh
@@ -1,22 +1,60 @@
 #!/bin/sh
-# this is scrapper written in pure POSIX shell because I'm linux purist who
-# doesn't touch grass
-
-# it extracts the clippings data from kindle and creates anki cards (in progress)
-
-# also the challenge is to not use awk
-# but idk maybe i will
-
+# AUTHOR: Jan Wiśniewki
+# LICENSED: WTFPL2 https://choosealicense.com/licenses/wtfpl/
+# CREATED ON: $ git log --reverse
+# COMMENTARY:
+#
+# This extracts the clippings data from kindle and creates anki cards
+#
+# EXAMPLE:
 # Here is how kindle clipping record looks like:
 # ==========
 # また、同じ夢を見ていた (住野よる)
 # - Your Highlight on page 3 | Location 14-14 | Added on Sunday, March 17, 2024 8:11:45 PM
 #
 # 向き合って
+# CODE:
 
-input="${1:-My Clippings.txt}"
+if [ "$1" = -h ]; then
+    cat 1>&2 <<-EOF
+    Skrypt extractuje zaznaczony tekst z ebooka (ktory znajduje sie w pliku "My Clippings")
+    a nastepenie przetwarza go do odpowiedniego formatu i kopiuje do schowka.
+    Nastepnym krokiem jest otworzenie przez uzytkownika texthookera oraz wlaczenie clipboard insertera
+    (rozszerzenie chrome) który kopiuje zawartość schowka do strony i pozwala użyc rozszerzenia Yomitan
+do podglodu 漢字/kanji (chińskie znaczki). Yomitan posiada funkcjonalność tworzenia "flashcardow"
+   korzystajac z programu "Anki".
+   Ten skomplikowanych process umożli mi szybkie uczenie sie nowych chińśkich znaczkow.
+   Yomitan: https://github.com/themoeway/yomitan
+   Clipboard inserter: https://github.com/themoeway/yomitan
+   Texthooker: https://anacreondjt.gitlab.io/texthooker.html
+   Anki: https://apps.ankiweb.net/
+EOF
+    exit
+fi
+
+if [ "$1" = -v ]; then
+    echo "version: 0.1" 1>&2;
+    exit
+fi
+
+# extract clippings from WSL
+input="My Clippings.txt"
+disk=$(zenity --title "Kindle Disk" --text "Enter disk letter" --entry) || exit
+if ! mkdir -p "/mnt/$disk" ; then
+    zenity --error --text "Can't create mounting point for kindle"
+    exit 1
+fi
+sudo -S -s <<EOF
+sudo -S umount "/mnt/$disk" &>/dev/null
+sudo -S mount -t drvfs "$disk": "/mnt/$disk"
+EOF
+if ! cp "/mnt/$disk/documents/My Clippings.txt" "./$input"; then
+    zenity --error --text "Can't extract clippings from kindle"
+    exit 1
+fi
+
 if ! [ -f "$input" ]; then
-    echo "file '$1' doesn't exist"
+    echo "file '$input' doesn't exist"
     exit 1
 fi
 dos2unix -q "$input"
@@ -59,8 +97,7 @@ EOF
     records=$(printf %s "$records")
 }
 
-sortbytitle()
-{
+sortbytitle() {
     records=""
     while IFS=$(printf "\t") read -r note title page loc date; do
 	mkrecord "$title" "$note" "$page" "$loc" "$date"
@@ -70,7 +107,7 @@ EOF
     records=$(printf %s "$records" | sort -s)
 }
 
-show() {
+show() {			# sexp format
     while IFS=$(printf "\t") read -r title note page loc date; do
 	test "$lasttitle" != "$title" && {
 	    test -n "$lasttitle" && printf ")\n"
@@ -87,8 +124,7 @@ EOF
 parse "$input"
 rmdups "$records"
 sortbytitle "$records"
-echo "$records" | xclip -sel clip
-exit
+zenity --info --title "Extraction complete" --text "Prepare texthooker and clipboard inserter"
 while read -r line; do
     echo "$line" | xclip -sel clip
     sleep 1
